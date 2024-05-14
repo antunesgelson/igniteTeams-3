759d451aafbf22924178d9d684d0c89185155094
Gelson <gelsondeveloper@hotmail.com>
2024-05-14 15:10:47 -0300

    -Interfaces, Navegação e Armazenamento local

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -1,7 +1,7 @@
-import { useState } from 'react';
-import { FlatList } from 'react-native';
+import { useCallback, useState } from 'react';
+import { Alert, FlatList } from 'react-native';
 
-import { useNavigation } from '@react-navigation/native';
+import { useFocusEffect, useNavigation } from '@react-navigation/native';
 
 import { Button } from '@components/Button';
 import { GroupCard } from '@components/GroupCard';
@@ -9,9 +9,12 @@ import { Header } from '@components/Header';
 import { Highlight } from '@components/Highlight';
 import { ListEmpty } from '@components/ListEmpty';
 
+import { Loading } from '@components/Loading';
+import { groupsGetAll } from '@storage/group/groupsGetAll';
 import { Container } from './styles';
 
 export function Groups() {
+    const [isLoading, setisLoading] = useState(true)
     const [groups, setGroups] = useState<string[]>([])
 
 
@@ -23,6 +26,37 @@ export function Groups() {
 
     }
 
+    async function fetchGroups() {
+        try {
+            setisLoading(true)
+
+            const data = await groupsGetAll()
+            setGroups(data)
+        }
+        catch (error) {
+            console.log(error);
+            Alert.alert('Turmas', 'Não foi possível carregar as turmas.')
+        }
+        finally {
+            setisLoading(false)
+
+        }
+    }
+
+    function handleOpenGroup(group: string) {
+        navigation.navigate('players', { group })
+    }
+
+
+
+    useFocusEffect(
+        useCallback(() => {
+            fetchGroups();
+
+        }, [])
+    );
+
+
     return (
         <Container  >
             <Header />
@@ -31,21 +65,22 @@ export function Groups() {
                 subtitle='jogue com sua turma'
             />
 
-
-            <FlatList
-                data={groups}
-                keyExtractor={item => item}
-                renderItem={({ item }) => (
-                    <GroupCard
-                        title={item}
-                    />
-                )}
-                contentContainerStyle={groups.length === 0 && { flex: 1 }}
-                ListEmptyComponent={() => (
-                    <ListEmpty message='Que tal cadastrar a primeira turma?' />
-                )}
-            />
-
+            {isLoading ? <Loading /> :
+                <FlatList
+                    data={groups}
+                    keyExtractor={item => item}
+                    renderItem={({ item }) => (
+                        <GroupCard
+                            onPress={() => handleOpenGroup(item)}
+                            title={item}
+                        />
+                    )}
+                    contentContainerStyle={groups.length === 0 && { flex: 1 }}
+                    ListEmptyComponent={() => (
+                        <ListEmpty message='Que tal cadastrar a primeira turma?' />
+                    )}
+                />
+            }
 
             <Button
                 title='Criar nova turma'
--- src/screens/Players/index.tsx
+++ src/screens/Players/index.tsx
@@ -1,3 +1,7 @@
+import { useRoute } from '@react-navigation/native';
+import { useEffect, useRef, useState } from 'react';
+import { Alert, FlatList, TextInput } from 'react-native';
+
 import { Button } from '@components/Button';
 import { ButtonIcon } from '@components/ButtonIcon';
 import { Filter } from '@components/Filter';
@@ -5,39 +9,146 @@ import { Header } from '@components/Header';
 import { Highlight } from '@components/Highlight';
 import { Input } from '@components/Input';
 import { ListEmpty } from '@components/ListEmpty';
+import { Loading } from '@components/Loading';
 import { PlayerCard } from '@components/PlayerCard';
-import { useNavigation } from '@react-navigation/native';
-import { useState } from 'react';
-import { FlatList } from 'react-native';
+import { AppError } from '@utils/AppError';
+
+import { groupRemoveByName } from '@storage/group/groupRemoveByName';
+import { PlayerStorageDTO } from '@storage/player/PlayerStorageDTO';
+import { playerAddByGroup } from '@storage/player/playerAddByGroup';
+import { playersGetByGroupAndTeam } from '@storage/player/playersGetByGroupAndTeam';
+import { playerRemoveByGroup } from '@storage/player/playersRemoveByGroup';
+
+
 import { Container, Form, HeaderList, NumberOfPlayers } from './styles';
 
+type RouteParams = {
+    group: string;
+}
+
 export function Players() {
-
+    const [isLoading, setisLoading] = useState(true)
+    const [newPlayerName, setNewPlayerName] = useState('')
     const [team, setTeam] = useState('Time A')
-    const [players, setPlayers] = useState<string[]>(['Gelson', 'Thiago', 'Hedgar'])
+    const [players, setPlayers] = useState<PlayerStorageDTO[]>([])
 
-    const navigation = useNavigation()
+    // const navigation = useNavigation()
 
-    function handleGoBack() {
-        navigation.navigate('groups')
+    const route = useRoute()
+    const { group } = route.params as RouteParams
+
+    const newPlayerNameRef = useRef<TextInput>(null)
+
+
+
+    async function handleAddPlayer() {
+        if (newPlayerName.trim().length === 0) {
+            return Alert.alert('Nova pessoa', 'Informe o nome da pessoa para adicionar.')
+
+        }
+
+
+        const newPlayer = {
+            name: newPlayerName,
+            team,
+        }
+
+
+        try {
+            await playerAddByGroup(newPlayer, group);
+            newPlayerNameRef.current?.blur()
+
+            setNewPlayerName('')
+            fetchPlayersByTeam()
+
+        }
+
+        catch (error) {
+            if (error instanceof AppError) {
+                Alert.alert('Nova pessoa', error.message)
+            } else {
+                Alert.alert('Nova pessoa', 'Não foi possível adicionar a pessoa.')
+
+            }
+        }
     }
-    
+
+
+
+
+    async function fetchPlayersByTeam() {
+        try {
+            setisLoading(true)
+            const playersByTeam = await playersGetByGroupAndTeam(group, team)
+            setPlayers(playersByTeam)
+        }
+        catch (error) {
+            console.log(error)
+            Alert.alert('Pessoas', 'Não foi possível carregar as pessoas do time selecionado.')
+        }
+        finally {
+            setisLoading(false)
+
+        }
+    }
+
+    async function handlePlayerRemove(playerName: string) {
+        try {
+            await playerRemoveByGroup(playerName, group)
+            fetchPlayersByTeam()
+        }
+        catch (error) {
+            console.log(error)
+            Alert.alert('Remover pessoa', 'Não foi possível remover a pessoa.')
+        }
+    }
+
+    async function groupRemove() {
+        try {
+            await groupRemoveByName(group)
+
+            // navigation.navigate('groups')
+
+        }
+        catch (error) {
+        }
+    }
+
+    async function handleGroupRemove() {
+        Alert.alert('Remover', 'Deseja realmente remover a turma?',
+            [
+                { text: 'Não', style: 'cancel' },
+                { text: 'Sim', onPress: () => groupRemove() },
+            ]
+        )
+    }
+
+    useEffect(() => {
+        fetchPlayersByTeam()
+    }, [team]);
+
 
     return (
         <Container>
             <Header showBackButton />
 
             <Highlight
-                title="Nome da turma"
+                title={group}
                 subtitle="adicine a galera e separe os times"
             />
 
             <Form>
                 <Input
+                    inputRef={newPlayerNameRef}
+                    onChangeText={setNewPlayerName}
+                    value={newPlayerName}
                     placeholder='Nome da pessoa'
                     autoCorrect={false}
+                    onSubmitEditing={handleAddPlayer}
+                    returnKeyType='done'
                 />
                 <ButtonIcon
+                    onPress={handleAddPlayer}
                     icon='add'
                 />
             </Form>
@@ -65,31 +176,34 @@ export function Players() {
                 </NumberOfPlayers>
             </HeaderList>
 
-            <FlatList
-                data={players}
-                keyExtractor={item => item}
-                renderItem={({ item }) => (
-                    <PlayerCard
-                        onRemove={() => { }}
-                        name={item}
-                    />
 
-                )}
-                ListEmptyComponent={() => (
-                    <ListEmpty
-                        message='Não há pessoas nesse time.'
-                    />
-                )}
+            {isLoading ? <Loading /> :
 
-                showsVerticalScrollIndicator={false}
-                contentContainerStyle={[{ paddingBottom: 100 }, players.length === 0 && { flex: 1 }]}
+                <FlatList
+                    data={players}
+                    keyExtractor={item => item.name}
+                    renderItem={({ item }) => (
+                        <PlayerCard
+                            onRemove={() => handlePlayerRemove(item.name)}
+                            name={item.name}
+                        />
 
-            />
+                    )}
+                    ListEmptyComponent={() => (
+                        <ListEmpty
+                            message='Não há pessoas nesse time.'
+                        />
+                    )}
 
+                    showsVerticalScrollIndicator={false}
+                    contentContainerStyle={[{ paddingBottom: 100 }, players.length === 0 && { flex: 1 }]}
+
+                />
+            }
             <Button
                 title='Remover Turma'
                 type='SECONDARY'
-                onPress={handleGoBack}
+                onPress={handleGroupRemove}
             />
 
         </Container>
